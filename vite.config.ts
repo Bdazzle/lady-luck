1e67e7b2464c9be52520aee261a35fceb4a1d83d
Bdazzle <bprobins1013@gmail.com>
2024-04-11 16:22:04 -0400

    dist
    dist for vite+pages?

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // base: "/Bdazzle/lady-luck/",
+  base: "/lady-luck/",
   
   plugins: [react()],
 })
