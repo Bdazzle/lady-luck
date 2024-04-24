4591afa39fab38c085d9502c662c2181237615f2
Bdazzle <bprobins1013@gmail.com>
2024-04-23 20:31:16 -0400

    meta
    meta tags

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
 import './index.css'
 import {
   createBrowserRouter,
-  // createHashRouter,
   RouterProvider,
 } from "react-router-dom";
 import Home from './pages/Home.tsx';
